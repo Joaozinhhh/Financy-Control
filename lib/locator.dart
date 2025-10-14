4e6fee6f634e629eeef4c610a12c1f08d5f809fc
João Vitor <joao.silva00555574@gmail.com>
2025-10-14 07:39:23 -0300

    dev: add get_it service locator initial setup

---
+++ lib/locator.dart
@@ -0,0 +1,9 @@
+import 'package:get_it/get_it.dart';
+
+final locator = GetIt.instance;
+
+void setupLocator() {
+  // Register your services and repositories here
+  // Example:
+  // locator.registerLazySingleton<YourService>(() => YourServiceImplementation());
+}
