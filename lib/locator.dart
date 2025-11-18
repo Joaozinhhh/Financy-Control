d8eaf576d1e6b3fb78983394a19cee6a37935ff8
Joaozinhhh <joao.silva00555574@gmail.com>
2025-11-18 20:17:43 -0300

    refactor: update locator to use storage service and mock repositories

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,16 +1,31 @@
+import 'package:financy_control/repositories/impl/mock_transaction_repository.dart';
+import 'package:financy_control/repositories/impl/mock_user_repository.dart';
+import 'package:financy_control/repositories/transaction_repository.dart';
+import 'package:financy_control/repositories/user_repository.dart';
 import 'package:financy_control/services/auth/auth_service.dart';
-import 'package:financy_control/services/auth/local_auth.dart';
-import 'package:financy_control/services/local_storage/local_storage_service.dart';
+import 'package:financy_control/services/auth/mock_auth_service.dart';
+import 'package:financy_control/services/storage/impl/local_storage_service.dart';
+import 'package:financy_control/services/storage/storage_service.dart';
 import 'package:get_it/get_it.dart';
 
 final locator = GetIt.instance;
 
 Future<void> setupLocator() async {
-  // Initialize local storage first
-  final localStorage = LocalStorageService();
-  await localStorage.init();
-  locator.registerLazySingleton<LocalStorageService>(() => localStorage);
+  // Initialize storage service first
+  final storageService = LocalStorageService();
+  await storageService.init();
+  locator.registerLazySingleton<StorageService>(() => storageService);
 
   // Register auth service
-  locator.registerLazySingleton<AuthService>(() => LocalAuth());
+  final authService = MockAuthService(storage: storageService);
+  await authService.init();
+  locator.registerLazySingleton<AuthService>(() => authService);
+
+  // Register repositories
+  locator.registerLazySingleton<TransactionRepository>(
+    () => MockTransactionRepository(storage: storageService),
+  );
+  locator.registerLazySingleton<UserRepository>(
+    () => MockUserRepository(storage: storageService),
+  );
 }
